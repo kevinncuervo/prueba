52a097aef56b496935196e56501634182c4d6a52
StivenHer <shernandez112767@umanizales.edu.co>
2025-05-21 20:28:58 -0500

    Add files via upload

---
+++ inventario.api/src/main/java/com/inventario/model/Cliente.java
@@ -0,0 +1,25 @@
+package com.inventario.model;
+
+public class Cliente {
+    private long id;
+    private String nombre;
+    private String identificacion;
+    private String correo;
+    private String celular;
+
+   
+    public long getId() { return id; }
+    public void setId(long id) { this.id = id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getIdentificacion() { return identificacion; }
+    public void setIdentificacion(String identificacion) { this.identificacion = identificacion; }
+
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+
+    public String getCelular() { return celular; }
+    public void setCelular(String celular) { this.celular = celular; }
+}
